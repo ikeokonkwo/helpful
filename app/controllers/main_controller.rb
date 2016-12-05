f9ee8752366d3b116ecda06849c8ad02380c111a
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-12-05 16:27:17 -0500

    Made API and used ajax call

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,11 @@
 class MainController < ApplicationController
 	def home
+		@f = Posting.find(1)
 		render :home
 	end
+
+	def getlocations
+		
+		render json:Posting.all 
+	end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+postingA = Posting.create!(title:"Brazilian Worker Center", description: "Social Justice Work", latitude: 42.355223, longitude: -71.132446)
+postingB = Posting.create!(title:"St.Petersburg General Hospital", description: "Hospital Setting Volunteer Experience", latitude: 27.805391, longitude: -82.726757)
+postingC = Posting.create!(title:"Massachusetts Alliance of Portuguese Speakers (MAPS)", description: "Uplifting the Portuguese Speaking Community in Boston", latitude: 42.316762, longitude: -71.064949)
+
+# create_table "postings", force: :cascade do |t|
+#     t.decimal  "latitude"
+#     t.decimal  "longitude"
+#     t.text     "description"
+#     t.integer  "user_id"
+#     t.string   "title"
+#     t.datetime "created_at",  null: false
+#     t.datetime "updated_at",  null: false
