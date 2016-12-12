caba3cd2544e848f5f8e95c5255f70233f0dc5c3
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2016-12-11 22:12:11 -0500

    searching

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 	delete "/postings/:id", to: 'postings#destroy', as: "delete_posting"
-	delete "/postings/:posting_id/reviews/:id", to: 'reviewss#destroy', as: "delete_review"
+	delete "/postings/:posting_id/review/:id", to: 'reviews#destroy', as: "delete_review"
 	
 	resources :postings do 
 			resources :reviews
 						end
 						
-	resources :details
+	
 
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
